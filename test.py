c9cf7ee6bec2e068692047b99480d4a63b8fd226
Jakob Kogler <jakob.kogler@gmail.com>
2016-06-26 22:09:05 +0200

    Add table class, that supports sorting, filtering, ...

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 from wca_api.wca_api import update_tsv_export, load
+from wca_api.table import table
 
 update_tsv_export()
-s = load('Persons', 'id name')
-t = load('Results', 'competitionId eventId roundId pos best average personName personCountryId formatId value1 value2 value3 value4 value5')
+s = table(load('Persons', 'id name'))
+t = table(load('Results', 'competitionId eventId roundId pos best average personName personCountryId formatId value1 value2 value3 value4 value5'))
 
-print(s[:10])
+print(s[0:5])
+print(getattr(s[0], 'id'))
+s.filter({'name': ['Jakob Kogler', 'Röhrer']})
+print(list(s))
 
-print(getattr(t[0], 'best'))
+print()
+
+t.filter({'personCountryId': 'Austria', 'eventId': ['444']})
+print(t[:3])
+t.sort('average')
+t.restrict_fields(['personName', 'average', 'competitionId'])
+print(t[-3:])
---
+++ wca_api/table.py
@@ -0,0 +1,42 @@
+from collections import namedtuple
+
+
+class table:
+    """Collection of rows of a table.
+    Supports easy filtering and sorting."""
+
+    def __init__(self, rows: list):
+        self._name = type(rows[0]).__name__ if rows else ''
+        self._rows = rows
+
+    def __getitem__(self, item):
+        return self._rows[item]
+
+    def __len__(self):
+        return len(self._rows)
+
+    def filter(self, conditions: dict):
+        if self._rows:
+            fields = self._rows[0]._fields
+            conditions = {field: allowed if isinstance(allowed, list) else [allowed]
+                          for field, allowed in conditions.items() if field in fields}
+            self._rows = [row for row in self._rows
+                          if all(getattr(row, field) in allowed for field, allowed in conditions.items())]
+
+    def sort(self, by, reverse=False):
+        if not isinstance(by, list):
+            by = [by]
+
+        if self._rows:
+            fields = self._rows[0]._fields
+            by = [name for name in by if name in fields]
+
+            self._rows.sort(key=lambda row: tuple(getattr(row, name) for name in by), reverse=reverse)
+
+    def restrict_fields(self, fields):
+        name = ''
+        if self._rows:
+            fields = [field for field in fields if field in self._rows[0]._fields]
+
+        Type = namedtuple(self._name, fields)
+        self._rows = [Type(*(getattr(row, field) for field in fields)) for row in self._rows]
