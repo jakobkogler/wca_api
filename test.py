d16b93228a88e688731853fa9ff9b4400629fb5b
Jakob Kogler <jakob.kogler@gmail.com>
2016-06-26 21:02:38 +0200

    Convert to package

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-from wca_api import update_tsv_export, load
+from wca_api.wca_api import update_tsv_export, load
 
 update_tsv_export()
 s = load('Persons', 'id name')
 t = load('Results', 'competitionId eventId roundId pos best average personName personCountryId formatId value1 value2 value3 value4 value5')
 
 print(s[:10])
+
+print(getattr(t[0], 'best'))
