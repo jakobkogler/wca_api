0c15a03e1900c2a28d07e9df622865033f4c1bb1
Jakob Kogler <jakob.kogler@gmail.com>
2016-06-27 17:36:11 +0200

    add py.test to setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup
 setup(
     name='wca_api',
     version='0.1dev',
+    setup_requires=['pytest-runner', ],
+    tests_require=['pytest', ],
     packages=['wca_api', ],
     license='MIT License',
     author='Jakob Kogler',
-    description='API for the WCA database'
+    description='API for the WCA database',
 )
