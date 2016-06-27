0382e85baa13bcca18653cfd32e64c3a2c1ba353
Jakob Kogler <jakob.kogler@gmail.com>
2016-06-27 17:31:22 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='wca_api',
+    version='0.1dev',
+    packages=['wca_api', ],
+    license='MIT License',
+    author='Jakob Kogler',
+    description='API for the WCA database'
+)
